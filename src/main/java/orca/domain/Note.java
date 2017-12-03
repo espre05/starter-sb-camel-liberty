e87a9506c8e0df36127248f489b33d5f44488434
espre05 <731599+espre05@users.noreply.github.com>
2017-12-03 02:59:14 -0500

    Update Note.java

--- src/main/java/orca/domain/Note.java
+++ src/main/java/orca/domain/Note.java
@@ -1,6 +1,6 @@
-package orca.domain;
-
+package telus.hns.bpcs.integration.domain;
 import java.io.Serializable;
+import java.sql.Date;
 import java.sql.Timestamp;
 
 import javax.persistence.Column;
@@ -11,75 +11,252 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.Immutable;
 
-
 @Entity
-@Table(name = "NOTES")
+@Table(name = "NOTETEXT")
 @Immutable
-@IdClass(NoteId.class)
-public class Note {
+@IdClass(NotetextId.class)
+public class Notetext {
     @Id
     @Column(name = "ID")
-    private int id;
+    private  String id;
     @Id
-    @Column(name = "code")
-    private Character code;
+    @Column(name = "code1")
+    private  String code1;
     @Id
-    @Column(name = "ins_ts")
-    private Timestamp insTs;
-    
-    
+    @Column(name = "code2")
+    private  String code2;   
+    @Id
     @Column(name = "note")
-    private String note;
+    private  String note; 
+    @Id
+    @Column(name = "code3")
+    private  String code3; 
+    @Id
+    @Column(name = "dt")
+    private  Date date; 
+    @Id
+    @Column(name = "ins_ts", nullable = true)
+    private  Timestamp insts; 
 
+    @Column(name = "ins_userid", nullable = true)
+    private  String insUser; 
+    
+    @Column(name = "upd_ts", nullable = true)
+    private  Timestamp updts; 
 
-    public int getId() {
-        return id;
-    }
+    @Column(name = "upd_userid", nullable = true)
+    private  String updUser;     
+    
+    @Column(name = "dt_valid_from")
+    private  Date dateValidFrom; 
+    
+    @Column(name = "dt_valid_to")
+    private  Date dateValidTo;
 
+	public String getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
+	public String getCode1() {
+		return code1;
+	}
 
-    public Character getCode() {
-        return code;
-    }
+	public void setCode1(String code1) {
+		this.code1 = code1;
+	}
 
+	public String getCode2() {
+		return code2;
+	}
 
-    public void setCode(Character code) {
-        this.code = code;
-    }
+	public void setCode2(String code2) {
+		this.code2 = code2;
+	}
 
+	public String getNote() {
+		return note;
+	}
 
-    public Timestamp getInsTs() {
-        return insTs;
-    }
+	public void setNote(String note) {
+		this.note = note;
+	}
 
+	public String getCode3() {
+		return code3;
+	}
 
-    public void setInsTs(Timestamp insTs) {
-        this.insTs = insTs;
-    }
+	public void setCode3(String code3) {
+		this.code3 = code3;
+	}
 
+	public Date getDate() {
+		return date;
+	}
 
-    public String getNote() {
-        return note;
-    }
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
+	public Timestamp getInsts() {
+		return insts;
+	}
 
-    public void setNote(String note) {
-        this.note = note;
-    }
+	public void setInsts(Timestamp insts) {
+		this.insts = insts;
+	}
 
+	public String getInsUser() {
+		return insUser;
+	}
 
- 
+	public void setInsUser(String insUser) {
+		this.insUser = insUser;
+	}
+
+	public Timestamp getUpdts() {
+		return updts;
+	}
+
+	public void setUpdts(Timestamp updts) {
+		this.updts = updts;
+	}
+
+	public String getUpdUser() {
+		return updUser;
+	}
+
+	public void setUpdUser(String updUser) {
+		this.updUser = updUser;
+	}
+
+	public Date getDateValidFrom() {
+		return dateValidFrom;
+	}
+
+	public void setDateValidFrom(Date dateValidFrom) {
+		this.dateValidFrom = dateValidFrom;
+	}
+
+	public Date getDateValidTo() {
+		return dateValidTo;
+	}
+
+	public void setDateValidTo(Date dateValidTo) {
+		this.dateValidTo = dateValidTo;
+	}
+
+	@Override
+	public String toString() {
+		return "Notetext [id=" + id + ", code1=" + code1 + ", code2=" + code2 + ", note=" + note + ", code3=" + code3
+				+ ", date=" + date + ", insts=" + insts + ", insUser=" + insUser + ", updts=" + updts + ", updUser="
+				+ updUser + ", dateValidFrom=" + dateValidFrom + ", dateValidTo=" + dateValidTo + "]";
+	} 
     
 }
 
-class NoteId implements Serializable {
+class NotetextId implements Serializable{
+    String id;
+    String code1;
+    String code2;   
+    String note; 
+    String code3; 
+    Date date; 
+    Timestamp insts; 
+}
 
-    private static final long serialVersionUID = 1L;
-    int id;
-    Character code;
-    Timestamp insTs;
+
+package telus.hns.bpcs.integration.domain;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Immutable;
+
+@Entity
+@Table(name = "TRACTIVITY")
+@Immutable
+public class ActivityCode {
+    @Id
+    @Column(name = "id")
+    private  int id;
+    @Column(name = "code1")
+    private  int code1;
+    @Column(name = "msg")
+    private  String msg;
+    
+    @Column(name = "dt")
+    private  Date date; 
+    
+    @Column(name = "ins_userid", nullable = true)
+    private  String insUser; 
+    @Column(name = "ins_ts", nullable = true)
+    private  Timestamp insts; 
+    
+    @Column(name = "upd_userid", nullable = true)
+    private  String updUser;  
+    @Column(name = "upd_ts", nullable = true)
+    private  Timestamp updts;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCode1() {
+		return code1;
+	}
+	public void setCode1(int code1) {
+		this.code1 = code1;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getInsUser() {
+		return insUser;
+	}
+	public void setInsUser(String insUser) {
+		this.insUser = insUser;
+	}
+	public Timestamp getInsts() {
+		return insts;
+	}
+	public void setInsts(Timestamp insts) {
+		this.insts = insts;
+	}
+	public String getUpdUser() {
+		return updUser;
+	}
+	public void setUpdUser(String updUser) {
+		this.updUser = updUser;
+	}
+	public Timestamp getUpdts() {
+		return updts;
+	}
+	public void setUpdts(Timestamp updts) {
+		this.updts = updts;
+	}
+	@Override
+	public String toString() {
+		return "ActivityCode [id=" + id + ", code1=" + code1 + ", msg=" + msg + ", date=" + date + ", insUser="
+				+ insUser + ", insts=" + insts + ", updUser=" + updUser + ", updts=" + updts + "]";
+	} 
+   
+
 }
